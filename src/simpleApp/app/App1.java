914dca13c82fb25fe4f3d5c4e5c608187c3c6e53
obol007 <obol007@gmail.com>
2020-05-27 21:23:18 +0200

    result set

--- src/simpleApp/app/App1.java
+++ src/simpleApp/app/App1.java
@@ -1,8 +1,6 @@
 package simpleApp.app;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App1 {
 
@@ -12,10 +10,26 @@ public class App1 {
                 .getConnection("jdbc:mysql://localhost:3306/mysql_db?serverTimezone=UTC",
                         "root", "coderslab")) {
             System.out.println("Connected successful");
-        } catch (Exception e) {
+            Statement statement = conn.createStatement();
+            String sqlQuery = "select * from employees";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String surname = resultSet.getString(2);
+                String name = resultSet.getString(3);
+                String email = resultSet.getString(4);
+                String department = resultSet.getString(5);
+                int salary = resultSet.getInt(6);
+                System.out.printf("%d. Employee: %s %s, Email: %s, Department: %s, Salary: %,d \n",id,name,surname,email,department,salary);
+            }
+
+        } catch (SQLException e) {
             System.out.println("Unable to connect");
             e.printStackTrace();
         }
+
+
 
 
     }
