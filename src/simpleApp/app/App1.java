678f06d82af320c813817281cca340fddf9a17fd
obol007 <obol007@gmail.com>
2020-05-27 20:55:10 +0200

    db connection

---
+++ src/simpleApp/app/App1.java
@@ -0,0 +1,23 @@
+package simpleApp.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App1 {
+
+    public static void main(String[] args) throws SQLException {
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql_db?serverTimezone=UTC", "root", "coderslab");
+            System.out.println("Connected successful");
+
+        }catch (Exception e){
+            System.out.println("Unable to connect");
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
